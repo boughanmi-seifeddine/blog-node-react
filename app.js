d75cbc0cb8e11ef7f6dfa50ddde78e8acb0631b7
boughanmi-seifeddine <boughanmi1.seifeddine@gmail.com>
2023-03-23 11:59:04 +0100

    test user endpoints

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express()
 
+const userRouter = require('./routes/userRoutes');
+// 3) ROUTES
+app.use('/api/v1/users', userRouter);
 module.exports = app;
---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+
+exports.getAllUsers = (req, res) => {
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    data: 'hello from all users'
+  });
+};
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  });
+};
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  });
+};
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  });
+};
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!'
+  });
+};
