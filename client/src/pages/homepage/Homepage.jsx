19dd4c055c2acb8422919f92760dde1074dfb608
boughanmi-seifeddine <boughanmi1.seifeddine@gmail.com>
2023-04-01 22:52:56 +0100

    get posts

--- client/src/pages/homepage/Homepage.jsx
+++ client/src/pages/homepage/Homepage.jsx
@@ -3,15 +3,28 @@ import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./homepage.css";
-
+import axios from 'axios'
+import React, {useEffect, useState} from "react";
 export default function Homepage() {
   const location = useLocation();
+    const [posts, setPosts] = useState([]);
   console.log(location);
+
+
+    useEffect( () => {
+        const getPosts = async () => {
+            const res = await axios.get('/api/v1/blogs')
+            setPosts(res.data.data.blogs)
+
+        };
+        getPosts()
+    }, []);
+    console.log(posts)
   return (
     <>
       <Header />
       <div className="home">
-          <Posts/>
+              <Posts posts={posts}/>
           <Sidebar/>
       </div>
     </>
--- client/src/pages/single/Single.jsx
+++ client/src/pages/single/Single.jsx
@@ -1,8 +1,23 @@
 import Sidebar from "../../components/sidebar/Sidebar";
 import SinglePost from "../../components/singlePost/SinglePost";
 import "./single.css";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import { useLocation } from "react-router";
 
 export default function Single() {
+    const [post, setPost] = useState([])
+    const {pathname} = useLocation()
+    const postId = pathname.split('/')[2]
+
+    useEffect(()=>{
+      const getPost = async ()=>{
+          const res = await axios.get(`/api/v1/blogs/${postId}`)
+          setPost(res.data.data.blog)
+      }
+        getPost()
+    },[postId])
+
   return (
     <div className="single">
       <SinglePost />
